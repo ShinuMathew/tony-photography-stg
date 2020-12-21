072afa551b8ff3e5c7c99504e0d91fde9f3acce6
Shinu Mathew <shinu.mathew@samsung.com>
2020-12-21 14:19:46 +0530

    Fixed all issues

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
-const btnScrollToTop = document.querySelector('#scrollToTopBtn');
+const btnScrollToTop = document.getElementById('scrollToTopBtn');
+const myFooter = document.getElementById('my-footer');
 const bodySpace = document.querySelector('.body-space');
 btnScrollToTop.addEventListener('click', function() {
     setTimeout(function () {
@@ -9,3 +10,13 @@ btnScrollToTop.addEventListener('click', function() {
         })
     },200);
 });
+window.addEventListener("wheel", displayScrollToTop)
+function displayScrollToTop() {
+    var triggerBottom = window.innerHeight / 2 * 6;
+    const top = myFooter.getBoundingClientRect().top
+    if(top < triggerBottom){        
+        btnScrollToTop.style.display = 'block'    
+    } else {
+        btnScrollToTop.style.display = 'none'
+    }
+}
